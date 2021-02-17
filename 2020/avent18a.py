f67053210bf485e8e1e7830e1c05598ecdbd320b
mborkar <mandar.borkar@gmail.com>
2021-02-16 23:41:46 -0800

    Upload latest

--- 2020/avent18a.py
+++ 2020/avent18a.py
@@ -1,31 +1,46 @@
 
 def calculate_value (operation):
-    value = int(operation[0])
-    for i in range (1, len(operation), 2):
-        if operation[i] == '+':
-            value = value + int(operation[i + 1])
-        elif operation[i] == '*':
-            value = value * int(operation[i + 1])
+
+    operation_index = [i for i in range(len(operation)) if operation.startswith('+', i)] + [i for i in range(len(operation)) if operation.startswith('*', i)]
+    operation_index.sort ()
+
+    value = int(operation[:operation_index[0]])
+
+    for i in range (0, len(operation_index)):
+        operator = operation[operation_index[i]]
+        if i+1 < len(operation_index) :
+            operand = operation[operation_index[i]+1:operation_index[i+1]]
+        else:
+            operand = operation[operation_index[i]+1:]
+
+        if operator == '+':
+            value = value + int(operand)
+        elif operator == '*':
+            value = value * int(operand)
+
     return value
 
-def find_operation (operation):
-    value = 0
-    i=0
-    j=0
-    int_op = []
-    print (operation)
-    print (len(operation))
-    while j < len (operation) :
-        j = j+1
-        if operation[j] == ')':
-            print('found op2=' + str(len(operation[1:j])) + ' ' + operation[1:j])
-            return operation[1:j]
-        elif operation[j] == '(':
-            int_op=find_operation(operation[j:])
-            value += calculate_value(int_op)
-            j = j + 2 + len(int_op)
-        print (str(j) + ' ' + operation[j:]+ ' ' + str(value))
+def process_parathesis (expression):
 
+    paranthesis_start = [i for i in range(len(expression)) if expression.startswith('(', i)]
+
+    if paranthesis_start == [] :
+        final_calculation = calculate_value(expression)
+        print ('final ' + expression + ' = ' + str(final_calculation))
+        return final_calculation
+
+    paranthesis_end = [i for i in range(len(expression)) if expression.startswith(')', i)]
+
+    j=0
+    while  paranthesis_end[0] > paranthesis_start[j] and j+1 < len(paranthesis_start):
+        j = j + 1
+    interim_value = calculate_value(expression[paranthesis_start[j]+1:paranthesis_end[0]])
+    print ('interim ' + expression[paranthesis_start[j]+1:paranthesis_end[0]] + ' = ' + str(interim_value))
+    interim_value = process_parathesis(expression[0:paranthesis_start[j]]+
+               str(interim_value)
+               +expression[paranthesis_end[0]+1:])
+
+# Main program
 
 f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2020/avent18input.txt", "r")
 operation = f1.readlines()
@@ -35,8 +50,7 @@ for i in range (0,len(operation)):
     operation[i] = operation[i].replace (' ','')
 #    operation[i] = operation[i].replace ('(','')
 #    operation[i] = operation[i].replace(')', '')
-print (operation[0])
 
-print('found op3=' + find_operation(operation[0][2:]))
-#final_value = calculate_value(operation[0])
-# print ( operation[0] + ' = ' + str(final_value))
+print (operation[0])
+final_calculation = process_parathesis (operation[0])
+print ('finalvalue ' + operation[0] + ' = ' + str(final_calculation))
