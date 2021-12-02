01c5e8d20d4e639bd86639b28024a407792e32bf
mborkar <mandar.borkar@gmail.com>
2021-12-01 22:20:21 -0800

    Upload latest

--- 2021/avent1b.py
+++ 2021/avent1b.py
@@ -3,8 +3,8 @@ mylist = f1.readlines()
 inc = 0;
 dec = 0;
 for i in range(0, len(mylist)-3):
-    depth1=int(mylist[i])+int(mylist[i+1])+int(mylist[i+2])
-    depth2=int(mylist[i+1])+int(mylist[i+2])+int(mylist[i+3])
+    depth1 = int(mylist[i])+int(mylist[i+1])+int(mylist[i+2])
+    depth2 = int(mylist[i+1])+int(mylist[i+2])+int(mylist[i+3])
     if depth2-depth1 > 0 :
         print ( 'depth=' + str ( depth1 ) + ' ' + str ( depth2 ) + ' increased.' );
         inc += 1;
---
+++ 2021/avent2a.py
@@ -0,0 +1,10 @@
+f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2021/avent2input.txt", "r")
+mylist = f1.readlines()
+inc = 0;
+for i in range(1, len(mylist)):
+    if int(mylist[i])-int(mylist[i-1]) > 0 :
+        print ( 'depth=' + str ( i ) + ' ' + str ( mylist[i] ) + ' increased.' );
+        inc += 1;
+    else:
+        print ( 'depth=' + str ( i ) + ' ' + str ( mylist[i] )  + ' decreased.' );
+print ('number of increase = ' + str(inc));
