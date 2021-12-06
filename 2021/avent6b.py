1b961ab85863686bd28cc0467dd27753afaf62c4
mborkar <mandar.borkar@gmail.com>
2021-12-06 11:23:19 -0800

    Upload latest

--- 2021/avent6b.py
+++ 2021/avent6b.py
@@ -1,29 +1,38 @@
-
-
 fishlist = []
+fishcountbyage = [0,0,0,0,0,0,0,0,0]
+fishcount = 0
 
 f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2021/avent6input.txt", "r")
 fileinput = f1.readlines()
 tempfishlist = fileinput[0].replace ( '\n', '' ).split ( "," )
-print (tempfishlist)
+
 for i in range (0,len(tempfishlist)) :
     fishlist.append (int(tempfishlist[i]))
 
-#fishlist.pop(0)
-print ( "Initial state "  + str ( fishlist ) )
-for days in range (1,19):
+print ("Fishes and their age initial state")
+print (fishlist)
 
-    addfish=0
-    for fish in range (0,len(fishlist)):
-        if fishlist[fish] == 0 :
-            fishlist[fish] = 6
-            addfish += 1
-        else:
-            fishlist[fish] -= 1
-    if addfish > 0 :
-        for i in range ( 0, addfish ):
-            fishlist.append ( 8 )
+for age in range (0,8) :
+    fishcountbyage[age] = fishlist.count(age)
 
-    print ( "After day " + str ( days ) ) # + ' ' + str ( fishlist ) )
+print ("Fish count by age initial state")
+print (fishcountbyage)
 
-print ('Total fish ' + str(len(fishlist)))
+for days in range (0,256):
+    age0 = fishcountbyage[0];
+    fishcountbyage[0] = fishcountbyage[1]
+    fishcountbyage[1] = fishcountbyage[2]
+    fishcountbyage[2] = fishcountbyage[3]
+    fishcountbyage[3] = fishcountbyage[4]
+    fishcountbyage[4] = fishcountbyage[5]
+    fishcountbyage[5] = fishcountbyage[6]
+    fishcountbyage[6] = fishcountbyage[7]+ age0
+    fishcountbyage[7] = fishcountbyage[8]
+    fishcountbyage[8] = age0
+
+print ('Fish count by age after 256 days')
+print (fishcountbyage)
+for x in fishcountbyage:
+    fishcount += x
+print ("Total fish count at end of 256 days : " + str(fishcount))
+# 1574445493136
