8b7fa067d95ef0ede8a67a7a5041a7fb6be46dca
mandarborkar <62719841+mandarborkar@users.noreply.github.com>
2021-12-27 18:22:42 -0800

    Latest files from Computer 2.

--- 2021/avent25a.py
+++ 2021/avent25a.py
@@ -1,38 +1,53 @@
+filename = '/Users/mborkar/PycharmProjects/adventofcode/2021/avent25input.txt'
 
-filename = '/Users/mborkar/PycharmProjects/adventofcode/2021/avent25testinput.txt'
+seac = [ list(x) for x in [x for x in open (filename).read().strip().split('\n') ]]
 
-h1 = [ list(x) for x in [x for x in open (filename).read().strip().split('\n') ]]
-totalses = 0
+xseac = len(seac[0])
+yseac = len(seac)
 
-for x in h1:
-    #print ('Main row '+str(x))
-    y = [ x for x in x]
-    #print (y)
-    i = 0
+for step in range (1,1000):
+    Found = False
+    currseac = []
+    for x in seac:
+        currseac.append (list(x))
 
-    while len(y) > 1 :
-        z = y[i] + y[i + 1]
-        if (y[i + 1] == '}' and y[i] != '{') or  (y[i+1] == ']' and y[i] != '[') or  (y[i+1] == ')' and y[i] != '(') or  ( y[i+1] == '>' and y[i] != '<'):
-            #print (y)
-            print('Found incorrect ' + str(y[i:i + 2]))
-            if (y[i+1]) == ')' :
-                totalses += 3
-            elif (y[i+1]) == ']' :
-                totalses += 57
-            elif (y[i+1]) == '}' :
-                totalses += 1197
-            elif (y[i+1]) == '>' :
-                totalses +=  25137
-            break
-        elif z == '[]' or z == '{}' or z == '()' or z == '<>':
-            #print('Found match popping them out ' + str(y[i:i + 1]))
-            y.pop(i)
-            y.pop(i)
-            i = -1
-        i += 1
-        if i >= len (y)-1 :
-            break;
+    for y in range (0, yseac):
+        if y == yseac-1:
+            ymoveto=0
+        else:
+            ymoveto=y+1
 
-print ('Total SES ' + str(totalses))
+        for x in range (0,xseac):
+            if x == xseac-1:
+                xmoveto=0
+            else:
+                xmoveto=x+1
 
+            if currseac[y][x] == '>' and currseac[y][xmoveto] == '.':
+                seac[y][xmoveto] = '>'
+                seac[y][x] = '.'
+                Found = True
+
+    currseac = []
+    for x in seac:
+        currseac.append (list(x))
+
+    for y in range (0, yseac):
+        if y == yseac-1:
+            ymoveto=0
+        else:
+            ymoveto=y+1
+        for x in range(0, xseac):
+            if x == xseac - 1:
+                xmoveto = 0
+            else:
+                xmoveto = x + 1
+            if currseac[y][x] == 'v' and currseac[ymoveto][x] == '.':
+                seac[ymoveto][x] = 'v'
+                seac[y][x] = '.'
+                Found = True
+
+    if not(Found):
+        print ('No change '+str(step))
+        break
 
