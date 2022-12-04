b5f6d206f5ee459cd529c87c8e6678a494d4794a
mandarborkar <62719841+mandarborkar@users.noreply.github.com>
2022-12-03 22:27:54 -0800

    Solved 2a.

--- 2022/advent2a.py
+++ 2022/advent2a.py
@@ -4,29 +4,29 @@
 
 # A for Rock, B for Paper, and C for Scissors
 # X for Rock, Y for Paper, and Z for Scissors
-# Rock (AX) defeats Scissors (BY), Scissors (CZ) defeats Paper (BY), and Paper (BY) defeats Rock (AX).
+# Win Rock (X) defeats Scissors (C), Scissors (Z) defeats Paper (B), and Paper (Y) defeats Rock (A).
+
+def calculatescore (play1, play2):
+    score=0
+    if play2=='X':
+        score=1
+    elif play2=='Y':
+        score=2
+    elif play2=='Z':
+        score=3
+
+    if ((play1=='C' and play2=='X') or (play1=='B' and play2=='Z') or (play1=='A' and play2=='Y')):
+        return score+6
+    elif (play1=='B' and play2=='Y') or (play1=='C' and play2=='Z') or (play1=='A' and play2=='X'):
+        return score+3
+    else:
+        return score+0
 
 f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2022/advent2input.txt", "r")
 inputlist = f1.readlines()
 
-# A Z - i am defeated so score=0+3
+myTotalScore=0
 
-myTotalScore = 0
-
-
-
-
-
-calorieslist = [0]
-
-
-j = 0
-for i in range(0, len(inputlist)):
-    if inputlist[i] == '\n':
-        j += 1
-        calorieslist.append(0)
-    else:
-        calorieslist[j] += int(inputlist[i].replace('\n', ''))
-    print(calorieslist[j])
-
-print("Elf no. " + str(calorieslist.index(max(calorieslist))) + " has max calories " + str(max(calorieslist)))
+for i in range (0,len(inputlist)):
+    myTotalScore += calculatescore(inputlist[i][0], inputlist[i][2])
+    print ('play = ' + inputlist[i] + ' current score = ' + str(myTotalScore))
