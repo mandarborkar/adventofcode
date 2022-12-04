cc3581c58c709b1e9d389d1c1aa632ea8f4bd681
mandarborkar <62719841+mandarborkar@users.noreply.github.com>
2022-12-03 17:45:51 -0800

    Solve 1a.

--- 2022/avent1a.py
+++ 2022/avent1a.py
@@ -3,13 +3,13 @@ calorieslist = [0]
 f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2022/avent1input.txt", "r")
 inputlist = f1.readlines()
 
-j=0
-for i in range (0,len(inputlist)):
-    if inputlist[i] == '\n' :
-        j +=1
+j = 0
+for i in range(0, len(inputlist)):
+    if inputlist[i] == '\n':
+        j += 1
         calorieslist.append(0)
     else:
         calorieslist[j] += int(inputlist[i].replace('\n', ''))
-    print (calorieslist[j])
+    print(calorieslist[j])
 
-print ("Elf no. " + str(calorieslist.index(max(calorieslist))) + " has max calories " + str(max(calorieslist)) )
+print("Elf no. " + str(calorieslist.index(max(calorieslist))) + " has max calories " + str(max(calorieslist)))
