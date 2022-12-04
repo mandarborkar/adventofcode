ad91cf8f0d2a5387ae20acc89d78cb43f65d9fba
mandarborkar <62719841+mandarborkar@users.noreply.github.com>
2022-12-04 11:40:16 -0800

    Solved 3a.

--- 2022/advent3a.py
+++ 2022/advent3a.py
@@ -1,42 +1,45 @@
 
-def findduplicate(runsack1, runsack2):
-    print (runsack1+runsack2)
-
-
-def calculatescore (play1, play2):
-    score=0
-    if play2=='X':
-        score=1
-    elif play2=='Y':
-        score=2
-    elif play2=='Z':
-        score=3
-
-    if ((play1=='C' and play2=='X') or (play1=='B' and play2=='Z') or (play1=='A' and play2=='Y')):
-        return score+6
-    elif (play1=='B' and play2=='Y') or (play1=='C' and play2=='Z') or (play1=='A' and play2=='X'):
-        return score+3
-    else:
-        return score+0
-
 f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2022/advent3input.txt", "r")
 inputlist = f1.readlines()
 
-myTotalScore=0
+myTotalScore = 0
 
-for i in range (0,3):
+for i in range (0,len(inputlist)):
     runsack = inputlist[i].replace('\n', '')
-    print(runsack)
+
     length = int(len(runsack)/2)
     runsack1 = list(runsack[0:length])
-    runsack2 = list(runsack[length+1:])
-    print (runsack1)
-    print (runsack2)
-    print (set(runsack1).intersection(runsack2))
-    common=''
-    common=str(set(runsack1).intersection(runsack2))
-    print (common)
-    print (ord(common[0]))
+    runsack2 = list(runsack[length:])
 
-print (ord('w'))
-print (ord('B')-65+27)
+    print('\ncounter = ' + str(i))
+    print(runsack)
+    print(runsack1)
+    print(runsack2)
+    print(list(set(runsack1) & set(runsack2)))
+    print('length = ' + str(len(list(set(runsack1) & set(runsack2)))))
+
+    if list(set(runsack1) & set(runsack2)) :
+        for j in range (0,len(list(set(runsack1) & set(runsack2)))):
+            common = ord(list(set(runsack1) & set(runsack2))[j])
+
+            if common > 91:
+                common -= 96
+            else:
+                common -= 38
+
+            myTotalScore += common
+
+            print(list(set(runsack1) & set(runsack2))[j])
+            print('Add to score ' + str(common))
+            print('Current score ' + str(myTotalScore))
+
+print('My final score ' + str(myTotalScore))
+
+
+print (ord('a')-96)
+print (ord('z')-96)
+print('')
+print (ord('A')-38)
+print (ord('Z')-38)
+
+
