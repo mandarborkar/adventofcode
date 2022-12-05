53d051d7fdfc95ce393bcbfb059dad17067394ae
mandarborkar <62719841+mandarborkar@users.noreply.github.com>
2022-12-04 16:55:51 -0800

    Solving 4a.

---
+++ 2022/advent4a.py
@@ -0,0 +1,9 @@
+
+f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2022/advent4testinput.txt", "r")
+inputlist = f1.readlines()
+
+myTotalScore = 0
+
+for i in range (0,len(inputlist)):
+    print(inputlist[i])
+
