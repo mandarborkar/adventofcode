8fe6888681d44eaaf0ab7c104c7392ac6fbc48b6
mandarborkar <62719841+mandarborkar@users.noreply.github.com>
2022-12-26 21:20:41 -0800

    Solved 5a.

--- 2022/advent5a.py
+++ 2022/advent5a.py
@@ -1,4 +1,3 @@
-
 '''
 [N]         [C]     [Z]
 [Q] [G]     [V]     [S]         [V]
@@ -10,25 +9,37 @@
 [T] [L] [D] [G] [P] [P] [V] [N] [R]
  1   2   3   4   5   6   7   8   9
 '''
-
-
-def fullycontained(fcs1, fce1, fcs2, fce2):
-    if ((fcs1 <= fcs2) and (fce1 >= fce2)) or ((fcs2 <= fcs1) and (fce2 >= fce1)):
-        print('contained')
-        return 1
-    return 0
-
-
-f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2022/advent4input.txt", "r")
+boxes = [['T','P','Z','C','S','L','Q','N'],
+         ['L','P','T','V','H','C','G'],
+         ['D','C','Z','F'],
+         ['G','W','T','D','L','M','V','C'],
+         ['P','W','C'],
+         ['P','F','J','D','C','T','S','Z'],
+         ['V','W','G','B','D'],
+         ['N','J','S','Q','H','W'],
+         ['R','C','Q','F','S','L','V']]
+f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2022/advent5input.txt", "r")
 inputlist = f1.readlines()
-totalcontained = 0
+mycommand = []
 for i in range(0, len(inputlist)):
-    print(inputlist[i].replace('\n', ''))
-    cs1 = int(inputlist[i].replace('\n', '').split(",")[0].split("-")[0])
-    ce1 = int(inputlist[i].replace('\n', '').split(",")[0].split("-")[1])
-    cs2 = int(inputlist[i].replace('\n', '').split(",")[1].split("-")[0])
-    ce2 = int(inputlist[i].replace('\n', '').split(",")[1].split("-")[1])
+# for i in range(0, 1):
+    mycommand.append(inputlist[i].replace('\n','').split())
+    moves = int(mycommand[-1][1])
+    source = int(mycommand[-1][3])-1
+    target = int(mycommand[-1][5])-1
+    print(moves)
+    print(source)
+    print(target)
+    for j in range(0, moves):
+        boxes[target].append(boxes[source][-1])
+        boxes[source].pop()
 
-    totalcontained += fullycontained(cs1, ce1, cs2, ce2)
+print(boxes)
 
-print(totalcontained)
+finalbottom = ''
+
+for i in range(0, len(boxes)):
+    finalbottom += boxes[i][-1]
+print(finalbottom)
+
+
