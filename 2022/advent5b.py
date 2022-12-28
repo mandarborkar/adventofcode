4d4758be90de06d3718a517e8a619125c694f8a0
mandarborkar <62719841+mandarborkar@users.noreply.github.com>
2022-12-27 17:00:44 -0800

    Solved 5b.

---
+++ 2022/advent5b.py
@@ -0,0 +1,55 @@
+
+'''
+[N]         [C]     [Z]
+[Q] [G]     [V]     [S]         [V]
+[L] [C]     [M]     [T]     [W] [L]
+[S] [H]     [L]     [C] [D] [H] [S]
+[C] [V] [F] [D]     [D] [B] [Q] [F]
+[Z] [T] [Z] [T] [C] [J] [G] [S] [Q]
+[P] [P] [C] [W] [W] [F] [W] [J] [C]
+[T] [L] [D] [G] [P] [P] [V] [N] [R]
+ 1   2   3   4   5   6   7   8   9
+
+Test
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+'''
+
+# boxes = [['Z', 'N'], ['M', 'C', 'D'], ['P']]
+
+boxes = [['T','P','Z','C','S','L','Q','N'],
+         ['L','P','T','V','H','C','G'],
+         ['D','C','Z','F'],
+         ['G','W','T','D','L','M','V','C'],
+         ['P','W','C'],
+         ['P','F','J','D','C','T','S','Z'],
+         ['V','W','G','B','D'],
+         ['N','J','S','Q','H','W'],
+         ['R','C','Q','F','S','L','V']]
+
+f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2022/advent5input.txt", "r")
+inputlist = f1.readlines()
+mycommand = []
+for i in range(0, len(inputlist)):
+# for i in range(0, 1):
+    mycommand.append(inputlist[i].replace('\n','').split())
+    moves = int(mycommand[-1][1])
+    source = int(mycommand[-1][3])-1
+    target = int(mycommand[-1][5])-1
+    print(moves)
+    print(source)
+    print(target)
+    for j in range(0, moves):
+        boxes[target].append(boxes[source][-moves+j])
+        boxes[source].pop(-moves+j)
+    print(boxes)
+
+finalbottom = ''
+
+for i in range(0, len(boxes)):
+    finalbottom += boxes[i][-1]
+print(finalbottom)
+
+
