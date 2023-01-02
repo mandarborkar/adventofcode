087914c15f3feea84897c2f372098355bbbefad1
mandarborkar <62719841+mandarborkar@users.noreply.github.com>
2023-01-02 09:37:32 -0800

    Solved 25a1.  Improving on base.

---
+++ 2022/advent25a1.py
@@ -0,0 +1,33 @@
+
+f1 = open("/Users/mborkar/PycharmProjects/adventofcode/2022/advent25input.txt", "r")
+inputlist = f1.read().strip().split()
+print (inputlist)
+revsnafulist = [x[::-1] for x in inputlist]
+print(revsnafulist)
+
+fueltotal = 0
+t = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "-": -1,
+    "=": -2
+}
+
+tx = {d:s for s,d in t.items()}
+for x in revsnafulist:
+    power = 1
+    for y in x :
+        fueltotal += (power * int(t[y]))
+        power *= 5
+print (fueltotal)
+
+x = fueltotal
+ans = ""
+while x > 0 :
+    y = ((x + 2) % 5) - 2
+    ans += str(tx[y])
+    x -= y
+    x //= 5
+
+print(ans[::-1])
