bd85e023c22ad697e4b584fc5ade8681c9c73a55
Mandar Borkar <mandar.borkar@gmail.com>
2025-08-09 10:03:34 -0700

    Checking to see if github is configured properly for vscode.

--- 2023/advent1a.py
+++ 2023/advent1a.py
@@ -11,9 +11,4 @@ for i in range(0, len(digitarr)):
     output1 = output.replace (' ', '')
     total += int(output1[0])*10+int(output1[-1])
 
-<<<<<<< HEAD
 print(total )
-=======
-print(total)
-print ('Hello world')
->>>>>>> eeaf5eaf714a0cd51a4c4794e85230e52e2a4ec4
